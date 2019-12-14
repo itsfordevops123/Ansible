b6309461292d1a8f46b459b9c55946b0d9cf3c2e
Hema Damacharla (PSP) <hdamacha@starbucks.com>
2019-12-14 14:59:49 -0800

    modified for loop

---
+++ testshutdown_updated.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+a="pawnee"
+b="shawnee"
+c="sioux"
+d= "httpd"
+home=/opt/gsit
+for i in a b c
+do
+    timeout 1300 $home/$i/tomee-bin/shutdown.sh
+    if [[ $? -eq 0 ]]
+        then
+        echo " Pawnee tomee application has stopped"
+        exit 0
+        else
+        echo "pawnee tomcat application errored"
+        exit 1
+    fi
+done
+for i in d
+do
+    timeout 1300 $home/$d/tomee-bin/shutdown.sh
+    if [[ $? -eq 0 ]]
+        then
+        echo " Apache httpd application has stopped"
+        exit 0
+        else
+        echo "Apache httpd application errored"
+        exit 1
+    fi
+done
+
