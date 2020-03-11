0146a4e66836ccb1a9769bfe69cbd36d4d1efdde
Hema Damacharla <hdamacha@starbucks.com>
2020-03-10 18:39:43 -0700

    updating json format

---
+++ script.js
@@ -0,0 +1,10 @@
+var jcontent = {
+    "Application": "Webapp1",
+    "Version": "1.0.0",
+    "Request_Count" : "100010000",
+    "Error_Count": "11111110",
+    "Success_Count": "1111111"     
+}
+var output = document.getElementById('output');
+
+output.innerHTML = jcontent.Application;
